b48d1c0fa541397f9d6670706427800ccf8ad798
nassarkareem2025-ship-it <nassarkareem2025@gmail.com>
2026-02-07 19:02:52 -0500

    Worker.java

--- Worker.java
+++ Worker.java
@@ -6,16 +6,45 @@ public class Worker extends Person {
         super(IDNum, firstName, lastname, title, YOB);
     }
 
+
+public void setHourlyPayRate(double rate){
+        this.hourlyPayRate = rate;
+}
+
+
+
     public double WeeklyPay(double hoursWorked){
-       return hourlyPayRate * hoursWorked;
+       if(hoursWorked <= 40) {
+           return hourlyPayRate * hoursWorked;
+       }
+       else{
+           double regPay = hourlyPayRate * 40;
+           double overtimePay = hourlyPayRate * 1.5 * (hoursWorked - 40);
+           return regPay + overtimePay;
+
+       }
     }
 
-    public void displayWeeklyPay(){
-        double pay = WeeklyPay(40);
-        System.out.println(fullName() + " earning $" + pay + " per week");
+    public void displayWeeklyPay(double hoursWorked){
+        double pay = WeeklyPay(hoursWorked);
+        if(hoursWorked <= 40) {
+            System.out.println(fullName() + " earning $" + pay + " per week");
+        }else{
+                double regPay = hourlyPayRate * 40;
+                double overtimeHours = hoursWorked - 40;
+                double overtimePay = hourlyPayRate * 1.5 * overtimeHours;
+                double totalPay = regPay + overtimePay;
+
+                System.out.printf("%s:%n", fullName());
+                System.out.printf("  Regular Hours: 40 at $%.2f = $%.2f%n", hourlyPayRate, regPay);
+                System.out.printf("  Overtime Hours: %.0f at $%.2f = $%.2f%n", overtimeHours, (hourlyPayRate * 1.5), overtimePay);
+                System.out.printf("  Total Pay: $%.2f%n", totalPay);
+        }
     }
 
-@Override
+
+
+    @Override
     public String toCSVRecord() {
         return super.toCSVRecord() + "," + hourlyPayRate;
     }
